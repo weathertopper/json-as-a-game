942d39367078caf7f9471dd32a9803b1a2de0bd4
weathertopper <nathanweatherly94@gmail.com>
2018-05-20 18:39:18 -0400

    minor refactor

--- front/js/buttons_and_keys.js
+++ front/js/buttons_and_keys.js
@@ -1,9 +1,7 @@
 $(document).keydown( (event) => {
-    // keys[event.keyCode] = true;
     setKeyCode(event.keyCode, true);
 })
 
 $(document).keyup( (event) => {
-    // delete keys[event.keyCode];
     setKeyCode(event.keyCode, false);
 })
